be87eab26dfb5f9618897e3464f17319bdccf523
SH4R1K <0504051@mail.ru>
2023-10-25 14:18:58 +0300

    start3

--- main.py
+++ main.py
@@ -15,9 +15,30 @@ from natasha import (
 
     Doc
 )
-
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer(lang="ru")
 segmenter = Segmenter()
-text = "Иван кот приехал в бот и уехал кот"
+
+emb = NewsEmbedding()
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+
+text = "Красная кошка Маруся приехала в бот и улетела на крюке кошке в стратосферу. Ботинки с кошками плотно зацепились за скалу."
 doc = Doc(text)
+
 doc.segment(segmenter)
-print(doc)
+doc.tag_morph(morph_tagger)
+doc.parse_syntax(syntax_parser)
+
+sent = doc.sents[0]
+#print(sent.syntax)
+#sent.syntax.print()
+#print(doc.spans[0])
+'''tokens = segmenter.tokenize(text)
+for token in tokens:
+    p = morph.parse(token.text)[0]
+    print(token)
+    print(p)
+    print('-----')
+    if (p.normal_form == "кошка"):
+        print(1)'''
