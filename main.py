448a05ee6ef37f8ac461c97476728ab63b7b2578
SH4R1K <0504051@mail.ru>
2023-10-26 14:10:10 +0300

    Added inputs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import releword as rw
 import model
-intents, predicts = model.model("text.txt", "model.txt")
+
+inputFileName = input("Введите имя файла с текстом для обработки: ")
+modelFileName = input("Введите имя файла с моделью обучения: ")
+
+intents, predicts = model.model(inputFileName, modelFileName)
 for i in range(len(intents)):
     print(f"{rw.find_phrases(intents[i])} - {predicts[i].strip()}")
 
